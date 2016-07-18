1a19902614efbb88988b21a6e573ad12d8f118d8
Spiros94 <cont@eyrhka.gr>
2016-07-18 13:26:09 +0300

    The main script for running the code

---
+++ main.m
@@ -0,0 +1,18 @@
+% Mesh and material constants
+n = [100 25 1]; %shape of the object
+dx = [5e-9 5e-9 3e-9]; %dimensions of individual magnets
+mu0 = 4e-7 * pi;
+gamma = 2.211e5;
+ms = 8e5;
+A = 1.3e-11;
+alpha = 0.02;
+
+global n_demag
+n_demag = zeros(n(1)*2-1,n(2)*2-1,n(3)*2-1,6);
+
+set_n_demag(1,[0 1 2], 'f');
+set_n_demag(2,[0 1 2], 'g');
+set_n_demag(3,[0 2 1], 'g');
+set_n_demag(4,[1 2 0], 'f');
+set_n_demag(5,[1 2 0], 'g');
+set_n_demag(6,[2 0 1], 'f');
