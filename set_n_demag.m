175fa9249e70a342fbc939275e7a758816bec730
Spiros94 <cont@eyrhka.gr>
2016-07-21 13:59:35 +0300

    Added basic functionality - iterations through all elements in the array - calculations using newells functions. Indexes need some review

--- set_n_demag.m
+++ set_n_demag.m
@@ -1,3 +1,25 @@
-function set_n_demag(iteration, permutation, func)
-
-end
+function set_n_demag(iteration, permut, func)
+    global n
+    global dx
+    global n_demag
+    binaryMatrix = de2bi([0:63], 6, 'left-msb'); % Specify a binary matric to get the sign for each element based on the sum of ones
+    for i = 1:n(1) % Nested loops to know the index
+        for j = 1:n(2)
+            for l = 1:n(3)
+                value = 0.0;
+                idx = zeros(1,3);
+                idx(1) = mod( (i + n(1) -1),(2*n(1)) ) - n(1) + 0;  % WARNING -- REVIEW MOD CALC
+                idx(2) = mod( (j + n(2) -1),(2*n(2)) ) - n(2) + 0;  % WARNING -- REVIEW MOD CALC  
+                idx(3) = mod( (l + n(3) -1),(2*n(3)) ) - n(3) + 0;  % WARNING -- REVIEW MOD CALC
+                for s = 1:size(binaryMatrix,1) % Iterate binary matrix
+                    x = (idx(permut(1)) + binaryMatrix(s,permut(1)) - binaryMatrix(s,permut(1)+3)) *dx(permut(1));
+                    y = (idx(permut(2)) + binaryMatrix(s,permut(2)) - binaryMatrix(s,permut(2)+3)) *dx(permut(2));
+                    z = (idx(permut(3)) + binaryMatrix(s,permut(3)) - binaryMatrix(s,permut(3)+3)) *dx(permut(3));
+                    value = value + ( (-1^sum(binaryMatrix(s,:))) * newells(func,x,y,z) );
+                end
+                n_demag(i,j,l,iteration) = - value / ( 4*pi*prod(dx) );
+            end % l loop
+        end % j  loop
+    end % i loop
+    
+end % Function
