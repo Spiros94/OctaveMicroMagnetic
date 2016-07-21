936375f1458853832317142f3bd6c87b74ee3dd3
Spiros94 <cont@eyrhka.gr>
2016-07-21 16:04:41 +0300

    Changed loops to match n_demag sizes and also fixed id indexes

--- set_n_demag.m
+++ set_n_demag.m
@@ -3,14 +3,14 @@ function set_n_demag(iteration, permut, func)
     global dx
     global n_demag
     binaryMatrix = de2bi([0:63], 6, 'left-msb'); % Specify a binary matric to get the sign for each element based on the sum of ones
-    for i = 1:n(1) % Nested loops to know the index
-        for j = 1:n(2)
-            for l = 1:n(3)
+    for i = 1:size(n_demag,1) % Nested loops to know the index
+        for j = 1:size(n_demag,2)
+            for l = 1:size(n_demag,3)
                 value = 0.0;
                 idx = zeros(1,3);
-                idx(1) = mod( (i + n(1) -1),(2*n(1)) ) - n(1) + 0;  % WARNING -- REVIEW MOD CALC
-                idx(2) = mod( (j + n(2) -1),(2*n(2)) ) - n(2) + 0;  % WARNING -- REVIEW MOD CALC  
-                idx(3) = mod( (l + n(3) -1),(2*n(3)) ) - n(3) + 0;  % WARNING -- REVIEW MOD CALC
+                idx(1) = mod( (i-1 + n(1) -1),(2*n(1)-1) ) - n(1) + 1;
+                idx(2) = mod( (j-1 + n(2) -1),(2*n(2)-1) ) - n(2) + 1;
+                idx(3) = mod( (l-1 + n(3) -1),(2*n(3)-1) ) - n(3) + 1;
                 for s = 1:size(binaryMatrix,1) % Iterate binary matrix
                     x = (idx(permut(1)) + binaryMatrix(s,permut(1)) - binaryMatrix(s,permut(1)+3)) *dx(permut(1));
                     y = (idx(permut(2)) + binaryMatrix(s,permut(2)) - binaryMatrix(s,permut(2)+3)) *dx(permut(2));
