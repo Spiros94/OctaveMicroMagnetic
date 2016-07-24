12ff5a26f4a5100bf97755e05e73596f37afad91
Spiros94 <cont@eyrhka.gr>
2016-07-24 23:48:40 +0300

    Added global alpha and f_n_demag variables.
    Changed the file to load for the n_demag variable to the mat exported by the matlab.
    
    Calculate the fft for the axes that are have size greater than 1.
    
    Create the table 'm' for later use.

--- main.m
+++ main.m
@@ -8,12 +8,14 @@ function main
     gamma = 2.211e5;
     ms = 8e5;
     A = 1.3e-11;
+    global alpha
     alpha = 0.02;
 
     global n_demag
     n_demag = zeros(n(1)*2-1,n(2)*2-1,n(3)*2-1,6);
     global m_pad
     m_pad = zeros(n(1)*2-1, n(2)*2-1, n(3)*2-1, 3);
+    global f_n_demag
 
     
     % Calculate demag tensor. Elements | Permutation | Function to be used
@@ -27,7 +29,19 @@ function main
     set_n_demag(6,[2 0 1]+1, 'f');
     %}
 
-    n_demag = load('n_demag.mat');
+    n_demag = load('n_demag_matlab.mat');
     n_demag = n_demag.n_demag;
+    
+    f_n_demag = n_demag;
+    for i = 1:size(n,2)
+       if n(i) > 1
+          f_n_demag = fft(f_n_demag, [], i); 
+       end
+    end
+    
+   m = zeros(n(1), n(2), n(3), 3);
+   m(n(1),:,:,2) = 1.0;
+   m(2:n(1)-1,:,:,1) = 1.0;
+    
     
 end
