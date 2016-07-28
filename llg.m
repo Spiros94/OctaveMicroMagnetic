de14b110f5a4f3085571e2c57742003e5a6543ac
Spiros94 <cont@eyrhka.gr>
2016-07-28 11:04:07 +0300

    Removed the return statment as the code uses the global variables(m, m_pad). Also removed the argument 'm' as the global 'm' is used and not a copy of the matrix.
    Commented all the return calculations as not needed for the time.

--- llg.m
+++ llg.m
@@ -1,15 +1,16 @@
-function rtn = llg(m, dt, h_zee)
+function llg(dt,h_zee)
     global gamma
     global alpha
-    
-    h = h_eff(m) + h_zee;
+    global m
+
+    h = h_eff() + h_zee;
     cros_m_h = cross(m, h, 4);
     term = (1+(alpha^2));
     dmdt = (-(gamma/term) * cros_m_h) - ((alpha*gamma)/term)*cross(m, cros_m_h, 4);
     m = m + dt*dmdt;
-    m_sum = sum(m.*m, 4);
-    m_sum_sqrt = sqrt(m_sum);  % Compute the square root of each element
-    m_sum_sqrt = reshape(m_sum_sqrt, [1 numel(m_sum_sqrt)]); % Flatten the array to a vector
-    m_sum_sqrt = [m_sum_sqrt m_sum_sqrt m_sum_sqrt]; % Repeat the vector 3 times to match m's size
-    rtn = m./reshape( m_sum_sqrt, size(m) );
+    %m_sum = sum(m.*m, 4);
+    %m_sum_sqrt = sqrt(m_sum);  % Compute the square root of each element
+    %m_sum_sqrt = reshape(m_sum_sqrt, [1 numel(m_sum_sqrt)]); % Flatten the array to a vector
+    %m_sum_sqrt = [m_sum_sqrt m_sum_sqrt m_sum_sqrt]; % Repeat the vector 3 times to match m's size
+    %rtn = m./reshape( m_sum_sqrt, size(m) );
 end
