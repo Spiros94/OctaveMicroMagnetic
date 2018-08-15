a2f39878bdbe97297dbdb56bbefe07d6c10120ad
unknown <cont@eyrhka.gr>
2018-08-15 22:13:22 +0300

    Added GUI support, removed output to file and instead the output is driven to GUI plot

--- main.m
+++ main.m
@@ -1,19 +1,36 @@
-function main
+function main(app)
+    global asp
+    asp = app; %Random variable name
+    statusLbl('Init');
+    
     % Mesh and material constants
     global n
-    n = [50 25 2]; %shape of the object
+    n = eval(app.ObjShape.Value); %shape of the object
+    app.ObjShape.Editable = 'off';
+    
     global dx
-    dx = [5e-9 5e-9 3e-9]; %dimensions of individual magnets
+    dx = eval(app.CellSize.Value); %dimensions of individual magnets
+    app.CellSize.Editable = 'off';
+    
     global mu0
-    mu0 = 4e-7 * pi;
+    mu0 = eval(app.mu0.Value);
+    app.mu0.Editable = 'off';
+    
     global gamma
-    gamma = 2.211e5;
+    gamma = app.Gamma.Value;
+    app.Gamma.Editable = 'off';
+
     global ms
-    ms = 8e5;
+    ms = app.ms.Value;
+    app.ms.Editable = 'off';
+    
     global A
     A = 1.3e-11;
+    
     global alpha
-    alpha = 1.0; % starts with 1 for relaxation
+    alpha = app.RelaxAlpha.Value;
+    app.RelaxAlpha.Editable = 'off';
+    
 
     global n_demag
     n_demag = zeros(n(1)*2-1,n(2)*2-1,n(3)*2-1,6);
@@ -21,7 +38,7 @@ function main
     m_pad = zeros(n(1)*2-1, n(2)*2-1, n(3)*2-1, 3);
     global f_n_demag
 
-    
+    statusLbl('Calculating N Demag');
     % Calculate demag tensor. Elements | Permutation | Function to be used
     set_n_demag(1,[0 1 2]+1, 'f');
     set_n_demag(2,[0 1 2]+1, 'g');
@@ -43,13 +60,22 @@ function main
    m(1,:,:,2) = 1.0;
    m(2:n(1)-1,:,:,1) = 1.0;
    
-   for i = 1:5000 % Call LLG 5000 times to relax
+   statusLbl('Relaxation');
+   statusLbl(strcat('Relaxation steps:',int2str(app.RelaxSteps.Value)));
+   
+   app.RelaxSteps.Editable = 'off';
+   
+   for i = 1:app.RelaxSteps.Value % Call LLG x times to relax
        llg(2e-13,0);  % args(m, dt, h_zee)
    end
-   
-   alpha = 0.02;
-   dt = 5e-15;
 
+   alpha = app.Alpha.Value;
+   app.Alpha.Editable = 'off';
+   
+   dt = app.StepSize.Value;
+   app.StepSize.Editable = 'off';
+    
+   statusLbl('Calculating Zeeman');
    h_zee = zeros(size(n));
    for i = 1:n(1)
       for j = 1:n(2)
@@ -61,11 +87,25 @@ function main
       end
    end
    
-   fid = fopen('output.txt', 'w');
+   statusLbl('Solving LLG');
+
+   output_buffer = [];
+   cnt = 0;
    for i = 0:(floor(1e-9/dt)-1)
-       output_buffer = [(i*1e9*dt) mean(reshape(m(:,:,:,1),1,[])) mean(reshape(m(:,:,:,2),1,[])) mean(reshape(m(:,:,:,3),1,[]))];
-       fprintf(fid, '%0.15f %0.15f %0.15f %0.15f \n', output_buffer);
+       output_buffer = [output_buffer; (i*1e9*dt) mean(reshape(m(:,:,:,1),1,[])) mean(reshape(m(:,:,:,2),1,[])) mean(reshape(m(:,:,:,3),1,[]))];
+       if mod(cnt,500) == 0 % draw graph every 500 steps
+           plot(app.UIAxes, output_buffer(:,1), output_buffer(:,2), output_buffer(:,1), output_buffer(:,3), output_buffer(:,1), output_buffer(:,4));
+           drawnow
+       end
+       cnt = cnt + 1;
        llg(dt,h_zee);
    end
+   statusLbl('Simulation Finished');
    beep
+   pause
+end
+
+function statusLbl(x)
+    global asp
+    asp.Label2.Text = x;
 end
